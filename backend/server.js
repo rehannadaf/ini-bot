4b790a0bc495adc738b033bdda6be6906354a950
Rehan Nadaf <nadafrehan536@gmail.com>
2026-08-28 21:27:53 +0530

    Fix frontend CORS connection

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
+  "https://ini-bot.onrender.com",
   "https://ini-bot-frontend.onrender.com"
 ];
 
